5fc0d42daedf5c397c29bc7858f9e5e49e760768
sowmyagr <sowmya.gr@gmail.com>
2026-01-07 12:41:22 +0000

    add enumerated to status

--- order-service/src/main/java/com/ecommerce/order/domain/Order.java
+++ order-service/src/main/java/com/ecommerce/order/domain/Order.java
@@ -26,6 +26,7 @@ public class Order {
     @Column(name = "customer_id", nullable = false)
     private String customerId;
 
+    @Enumerated(EnumType.STRING)
     @Column(name="order_status", nullable = false)
     private OrderStatus status;
 
